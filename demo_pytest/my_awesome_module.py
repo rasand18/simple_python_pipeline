016a4df8fbc798cbdc32f8b831ae42798a322b7f
Rasmus <rasmusandreasson@outlook.com>
2021-08-05 12:45:38 +0200

    lagg

---
+++ demo_pytest/my_awesome_module.py
@@ -0,0 +1,15 @@
+
+
+def bin_number_to_tens(input_value: float) -> str:
+    if input_value < 0:
+        raise NotImplementedError("Not implemented for values below zero!")
+    elif input_value < 10:
+        return "[0-10)"
+    elif input_value < 20:
+        return "[10-20)"
+    elif input_value < 30:
+        return "[20-30)"
+    else:
+        raise NotImplementedError("Not implemented for values above 30!")
+
+
---
+++ demo_pytest/test_my_awesome_module.py
@@ -0,0 +1,83 @@
+from my_awesome_module import bin_number_to_tens
+import pytest
+
+"""
+To run:
+
+0. Make sure pytest is installed, either globally or in a virtualenv, and that virtualenv is activated
+1. cd into this folder
+2. Run: python -m pytest test_my_awesome_module.py
+
+"""
+
+def test_this_will_always_crash():
+    # Assert:
+    assert False
+
+
+def test_this_will_always_pass():
+    # Assert:
+    assert True
+
+
+def test_bin_my_number():
+    # Arrange:
+    input_value = 10
+
+    # Act:
+    result = bin_number_to_tens(input_value)
+
+    # Assert:
+    assert result == "[10-20)"
+
+
+def test_bin_my_number_FAILING():
+    # Arrange:
+    input_value = "This input will crash the function"
+
+    # Act:
+    result = bin_number_to_tens(input_value)
+
+    # Assert:
+    assert result == "[10-20)"
+
+
+@pytest.mark.parametrize(
+    "input_value,expected",
+    [
+        (0, "[0-10)"),
+        (1, "[0-10)"),
+        (2, "[0-10)"),
+        (3, "[0-10)"),
+        (4, "[0-10)"),
+        (5, "[0-10)"),
+        (15, "[10-20)"),
+        (25, "[20-30)"),
+    ]
+)
+def test_bin_over_many_inputs_by_using_parameterize(input_value, expected):
+    # Arrange is already done, oh yeah!
+
+    # Act    
+    result = bin_number_to_tens(input_value)
+
+    # Assert
+    assert result == expected
+
+
+def test_assert_raising_error():
+    # Arrange:
+    input_value = -10
+
+    # Act and Assert:
+    with pytest.raises(NotImplementedError) as my_error:
+        result = bin_number_to_tens(input_value)
+
+
+def test_assert_raising_error_FAILING():
+    # Arrange:
+    input_value = 10
+
+    # Act and Assert:
+    with pytest.raises(NotImplementedError) as my_error:
+        result = bin_number_to_tens(input_value)
